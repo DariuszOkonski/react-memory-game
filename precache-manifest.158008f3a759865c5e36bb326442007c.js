0b67305aa52d9c92920035bd09d89346b90be26f
Dariusz Okonski <darek200180@gmail.com>
2019-12-14 21:37:45 +0000

    Updates

---
+++ precache-manifest.158008f3a759865c5e36bb326442007c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecca97d1d39e6495cab9e3221e307c48",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "e7c2f4f57271de696a7c",
+    "url": "/react-memory-game/static/css/main.94db2cbd.chunk.css"
+  },
+  {
+    "revision": "2283c21aa9b4db938913",
+    "url": "/react-memory-game/static/js/2.d79422d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-memory-game/static/js/2.d79422d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7c2f4f57271de696a7c",
+    "url": "/react-memory-game/static/js/main.c8ef6f83.chunk.js"
+  },
+  {
+    "revision": "39d7614c46f0a43dcd52",
+    "url": "/react-memory-game/static/js/runtime-main.e1da31fd.js"
+  }
+]);
