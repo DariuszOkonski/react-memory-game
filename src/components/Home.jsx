aafe76f30d39a1fdb8ff35b838adea56d21a94b6
Dariusz Okonski <darek200180@gmail.com>
2019-12-14 22:58:50 +0000

    add jumbotron to component Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+
+const Home = () => {
+  return (
+    <div className="jumbotron bg-secondary text-light">
+      <h1 className="display-5">Memory Game</h1>
+      <p className="lead">Let's play</p>
+      <hr className="my-4" />
+      <NavLink className="btn btn-primary btn-sm mr-2" to="/game" role="button">Game</NavLink>
+      <NavLink className="btn btn-primary btn-sm" to="/about" role="button">About</NavLink>
+    </div>
+  );
+}
+
+export default Home;
