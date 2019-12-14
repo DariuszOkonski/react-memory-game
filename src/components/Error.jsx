66ffab5aaa5f4f2ba7da539a36a11b979753839c
Dariusz Okonski <darek200180@gmail.com>
2019-12-14 23:05:09 +0000

    add jumbotron to Error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <h1>Error</h1>
+    <div className="jumbotron bg-danger text-light">
+      <h1 className="display-5">Error - there is no such a route</h1>
+      <p className="lead">choose one of the options below</p>
+      <hr className="my-4" />
+      <NavLink className="btn btn-primary btn-sm mr-2" exact to="/" role="button">Home</NavLink>
+      <NavLink className="btn btn-primary btn-sm mr-2" to="/game" role="button">Game</NavLink>
+      <NavLink className="btn btn-primary btn-sm" to="/about" role="button">About</NavLink>
+    </div>
   );
 }
 
