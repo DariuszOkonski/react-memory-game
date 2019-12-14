d89487b4f02851cced626c04462b960c3737d2bf
Dariusz Okonski <darek200180@gmail.com>
2019-12-14 22:33:08 +0000

    change a tags to NavLink

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
       <div className="container">
 
-        <a className="navbar-brand" href="#">DazzDev</a>
+        <NavLink className="navbar-brand" exact to="/">DazzDev</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -13,13 +14,13 @@ const Navigation = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#">Home</a>
+              <NavLink className="nav-link" exact to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Game</a>
+              <NavLink className="nav-link" to="/game">Game</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              <NavLink className="nav-link" to="/about">About</NavLink>
             </li>
           </ul>
         </div>
