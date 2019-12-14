878db8b6262e6b00446a23ea558037a89a266d26
Dariusz Okonski <darek200180@gmail.com>
2019-12-14 23:01:08 +0000

    add Switch and Route components with setting routing for all paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Game from './components/Game';
+import About from './components/About';
+import Error from './components/Error';
 
 function App() {
   return (
     <div>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navigation />
+        <div style={{ marginTop: '4rem' }}></div>
 
         <section style={{ backgroundColor: 'yellow' }} className="container mt-4">
-
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/game" component={Game} />
+            <Route path="/about" component={About} />
+            <Route component={Error} />
+          </Switch>
         </section>
 
         <Footer />
