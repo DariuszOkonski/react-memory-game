c42eafa374c54f73491eb2f2b6820f31469a65d0
Dariusz Okonski <darek200180@gmail.com>
2019-12-14 22:32:09 +0000

    add BrowserRouter and section container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navigation />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Navigation />
 
-      <Footer />
+        <section style={{ backgroundColor: 'yellow' }} className="container mt-4">
+
+        </section>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
