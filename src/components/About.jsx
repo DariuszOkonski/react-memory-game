42a4e58b0f198aedbd3679d8366d38fea20f2d12
Dariusz Okonski <darek200180@gmail.com>
2019-12-14 23:13:00 +0000

    add card to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 const About = () => {
   return (
-    <h1>About</h1>
+    <div>
+      <div className="card bg-secondary text-light" style={{ width: '100%' }}>
+        <div className="card-header h2">
+          About
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item bg-primary">Version: 1.0.0</li>
+          <li className="list-group-item bg-primary">Author: Dariusz Okonski</li>
+          <li className="list-group-item bg-primary">Year: 2019</li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
