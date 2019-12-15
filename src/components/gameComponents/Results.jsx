20bdeaa9e9b2b70cb69a6e8a81c71965ab021bb7
Dariusz Okonski <darek200180@gmail.com>
2019-12-15 17:29:53 +0000

    add prop steps

--- src/components/gameComponents/Results.jsx
+++ src/components/gameComponents/Results.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Results = () => {
+const Results = ({ steps }) => {
   return (
     <div className="card bg-secondary text-light text-center" style={{ width: '100%' }}>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item bg-primary">Number of steps: 23</li>
+        <li className="list-group-item bg-primary">Number of steps: {steps}</li>
         <li className="list-group-item bg-primary">Time: 02:20</li>
       </ul>
     </div>
