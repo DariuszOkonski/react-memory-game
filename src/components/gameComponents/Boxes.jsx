ad124d21241c27d8477af31c352f44748a5e3b0f
Dariusz Okonski <darek200180@gmail.com>
2019-12-15 15:44:59 +0000

    add functions openCards, openFirstCard, openSecondCard, disableAllClickabel, componentDidUpdate

--- src/components/gameComponents/Boxes.jsx
+++ src/components/gameComponents/Boxes.jsx
@@ -1,25 +1,105 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Box from './Box';
 import { createBoxes } from './utilities';
 
-const Boxes = (props) => {
-  const [boxesArr, setBoxexArr] = useState(createBoxes(props.cards));
+class Boxes extends Component {
+  state = {
+    boxesArr: createBoxes(this.props.cards),
+    firstOpen: false,
+    secondOpen: false,
+    firstColor: '',
+    secondColor: '',
+  }
 
-  console.log(boxesArr);
-  return (
-    props.cards === 0 ?
-      <div className="card bg-danger text-light h5 text-center">
-        <div className="card-body">
-          Nothing to display. Choose number of cards
-        </div>
-      </div>
-      :
-      <div className="playBoard">
-        {
-          boxesArr.map(box => <Box key={box.id} {...box} />)
+  openCards = (id, color) => {
+    // console.log(`id: ${id} - color: ${color}`);
+    this.openFirstCard(id, color);
+
+    if (this.state.firstOpen) {
+      this.openSecondCard(id, color);
+      this.disableAllClickable();
+    }
+  }
+
+  openFirstCard = (id, color) => {
+    if (!this.state.firstOpen) {
+      let boxesArr = [...this.state.boxesArr];
+      boxesArr.forEach(box => {
+        if (box.id === id) {
+          box.displayBg = false;
+          box.clickable = false;
         }
-      </div>
-  );
+      })
+
+      this.setState({
+        boxesArr,
+        firstOpen: true,
+        firstColor: color,
+      })
+    }
+  }
+
+  openSecondCard = (id, color) => {
+    if (!this.state.secondOpen) {
+      let boxesArr = [...this.state.boxesArr];
+      boxesArr.forEach(box => {
+        if (box.id === id) {
+          box.displayBg = false;
+          box.clickable = false;
+        }
+      })
+
+      this.setState({
+        boxesArr,
+        secondOpen: true,
+        secondColor: color,
+      })
+    }
+  }
+
+  disableAllClickable = () => {
+    let boxesArr = [...this.state.boxesArr];
+    boxesArr.forEach(box => box.clickable = false);
+
+    this.setState({
+      boxesArr
+    })
+  }
+
+  componentDidUpdate() {
+    if (this.state.firstOpen && this.state.secondOpen) {
+      if (this.state.firstColor === this.state.secondColor) {
+        console.log('colors are equal');
+      } else {
+        console.log('colors are not equal');
+      }
+    }
+  }
+
+
+
+  render() {
+    console.log(this.state.boxesArr)
+    return (
+      this.props.cards === 0 ?
+        <div className="card bg-danger text-light h5 text-center">
+          <div className="card-body">
+            Nothing to display. Choose number of cards
+        </div>
+        </div>
+        :
+        <div className="playBoard">
+          {
+            this.state.boxesArr.map(box =>
+              <Box
+                key={box.id}
+                {...box}
+                openCards={this.openCards}
+              />)
+          }
+        </div>
+    );
+  }
 }
 
 export default Boxes;
